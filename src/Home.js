f70b6e76601dbfc55c6c740a2c0d141ffe9991e2
Mavi Bucheli <mavibucheli@gmail.com>
2021-12-03 13:10:51 -0500

    added icon to about me

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,9 @@ function Home() {
             style={{width: "40vw", height: "40vh", backgroundColor: "#e1dee9", color: "black", borderRadius: "10px", fontFamily: "Domine", paddingTop: "5px"}}>
                 <h5 style={{fontWeight: "bolder"}}>About Me</h5>
                 <p style={{marginTop: "70px"}}>A multilingual Software Engineer who loves front-end development and solving puzzles. Front-end development, like puzzles, demands that everything connects in order to be solved. Delivering any solution is immensely rewarding and truly motivating. Drawn to UI, committed to client satisfaction and user experience.</p>
+                <i className="large material-icons" style={{textAlign: "center", fontSize: "30px", color: "#a09abc", paddingTop: "20px"}}>add_circle</i>
             </Link>
-        </div>
+            </div>
     );
 };
 
