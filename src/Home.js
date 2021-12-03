69370f50bd23917ca0e40ada825358be811a4ef4
Mavi Bucheli <mavibucheli@gmail.com>
2021-12-03 15:55:32 -0500

    edited styling for about me

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,9 @@ function Home() {
         <div style={{textAlign: "center", marginTop: "250px", marginBottom: "100px"}}>
             <Link to="/about" 
             className="waves-effect waves-light btn pulse" 
-            style={{width: "40vw", height: "40vh", backgroundColor: "#e1dee9", color: "black", borderRadius: "10px", fontFamily: "Domine", paddingTop: "5px"}}>
-                <h5 style={{fontWeight: "bolder"}}>About Me</h5>
-                <p style={{marginTop: "70px"}}>A multilingual Software Engineer who loves front-end development and solving puzzles. Front-end development, like puzzles, demands that everything connects in order to be solved. Delivering any solution is immensely rewarding and truly motivating. Drawn to UI, committed to client satisfaction and user experience.</p>
+            style={{width: "55vw", height: "45vh", backgroundColor: "#e1dee9", color: "black", borderRadius: "10px", fontFamily: "Domine", paddingTop: "15px"}}>
+                <h5 style={{fontWeight: "bolder", marginTop: "70px"}}>About Me</h5>
+                <p style={{marginTop: "40px"}}>A multilingual Software Engineer who loves front-end development and solving puzzles. Front-end development, like puzzles, demands that everything connects in order to be solved. Delivering any solution is immensely rewarding and truly motivating. Drawn to UI, committed to client satisfaction and user experience.</p>
                 <i className="large material-icons" style={{textAlign: "center", fontSize: "30px", color: "#a09abc", paddingTop: "20px"}}>add_circle</i>
             </Link>
             </div>
